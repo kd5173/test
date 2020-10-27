c8936481da9525b466a31501446f1a82505da57e
molm <molm@DESKTOP-KQKO3UE>
2020-10-27 11:27:46 +0800

    .

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -5,14 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	   @GetMapping("/echo")
-	    
-	    public String echo(String name) {
-	        // System.out.println("echo get...");
-	        String echo = "hi," + name;
-	        System.out.println(echo);
-	        String web="hello,"+name;
-	        System.out.println(web);
-	        return echo;
-	    }
+	
+	 private static String address;
+	 
+	 private static String email;
+	 
+	 private int age = 20;
+	
+     @GetMapping("/echo")
+     public String echo(String name) {
+        // System.out.println("echo get...");
+    	
+    	address=name + " have address ";
+    	email=name + " have email ";
+    	age =21;
+    	
+    	name = name+" jerry"; 
+        String echo = "hi," + name;
+        System.out.println(echo);
+        String web="hello,"+name;
+        System.out.println(web);
+        return echo;
+     }
 }
